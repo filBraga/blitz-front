920f6a1fbdec7db21803bccd325d9a05e13acbf0
Filipe <filipe.b.braga@gmail.com>
2022-07-01 18:31:03 -0300

    adding new tasks component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Title from './components/Title'
 import Task from './components/Task'
+import NewTask from './components/NewTask'
 
 function App() {
   return (
@@ -13,7 +14,19 @@ function App() {
       <div className='DefaultComponent'>
         <Task text='Do front-end App' />
       </div>
-      
+
+      <div className='DefaultComponent'>
+        <Task text='Do back-end App' />
+      </div>
+
+      <div className='DefaultComponent'>
+        <Task text='Lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum' />
+      </div>
+
+      <div className='DefaultComponent'>
+        <NewTask />
+      </div>
+
     </div>
   );
 }
