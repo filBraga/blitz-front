df9e69df57648daeb96364b3d934d0a1d813b4ff
Filipe <filipe.b.braga@gmail.com>
2022-07-01 19:55:44 -0300

    push to github

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -4,7 +4,7 @@ export default function Task(props) {
   return (
   <>
     <h4>New task</h4>
-    <textarea rows="4" cols="35" style={{ resize:'none'}}> </textarea>
+    <textarea rows="4" cols="35" style={{ resize:'none'}}>Type here</textarea>
     <br />
     <Button buttonText='Add'/>
   </>
