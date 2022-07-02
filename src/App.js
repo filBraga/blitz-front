94b5f5aa4b70df97edfa8c63195d63b7c69ab116
Filipe <filipe.b.braga@gmail.com>
2022-07-01 23:14:05 -0300

    connecting front with back

--- src/App.js
+++ src/App.js
@@ -2,33 +2,55 @@ import './App.css';
 import Title from './components/Title'
 import Task from './components/Task'
 import NewTask from './components/NewTask'
+import React from 'react';
 
-function App() {
-  return (
-    <div className='App'>
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={apiResponse: ''}
+  }
 
-      <div className='DefaultComponent'>
-        <Title text='To do test' />
+  callApi(){
+    fetch('http://localhost:9000/testApi')
+      .then(res => res.text())
+      .then(res => this.setState({apiResponse: res}));
+  }
+
+  componentDidMount(){
+    this.callApi();
+  }
+
+  render() {
+    return (
+      <div className='App'>
+
+        <div className='DefaultComponent'>
+          <Title text={this.state.apiResponse} />
+        </div>
+
+        <div className='DefaultComponent'>
+          <Title text='To do test' />
+        </div>
+
+        <div className='DefaultComponent'>
+          <Task text='Do front-end App' />
+        </div>
+
+        <div className='DefaultComponent'>
+          <Task text='Do back-end App' />
+        </div>
+
+        <div className='DefaultComponent'>
+          <Task text='Lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum' />
+        </div>
+
+        <div className='DefaultComponent'>
+          <NewTask />
+        </div>
+
       </div>
-
-      <div className='DefaultComponent'>
-        <Task text='Do front-end App' />
-      </div>
-
-      <div className='DefaultComponent'>
-        <Task text='Do back-end App' />
-      </div>
-
-      <div className='DefaultComponent'>
-        <Task text='Lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum' />
-      </div>
-
-      <div className='DefaultComponent'>
-        <NewTask />
-      </div>
-
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
