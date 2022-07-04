1e1fdbaf780eb6b50c120c23a4a7d344056bc69f
Filipe <filipe.b.braga@gmail.com>
2022-07-04 14:45:18 -0300

    adding delete button function

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,27 @@
-import Button from "./Button"
+import React, { useContext } from 'react'
+import AppContext from '../context/AppContext'
 
 export default function Task(props) {
+
+  const { tasks, setTasks } = useContext(AppContext);
+
+  const deleteTodo = (tasks, taskId) => {
+    const newTasks = tasks.filter(item => item.id !== taskId);
+    setTasks(newTasks);
+  }
+  
     return (
-      <>
+      <div>
+
         <p>{props.taskId}. {props.text}</p>
-        <Button buttonText='Delete' />
-        <Button buttonText='Update'/>
-        <Button buttonText='Done'/>
-      </>
+
+        <button onClick={() => deleteTodo(tasks, props.taskId)}>Delete</button>
+
+        <button>Update</button>
+
+        <button>Done</button>
+
+      </div>
     );
   
   
